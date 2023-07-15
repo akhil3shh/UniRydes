ed95cbf6ce01af4a84f7198e7b1d883b4d28c107
Akhi <akhileshnadgiri@gmail.com>
2023-07-15 18:36:07 +0530

    Added JS files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_jeLk2MmBe', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5p9r0a6hrseg34veh3pnp06mit', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://asb8mhp8q1.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
